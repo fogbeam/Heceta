5710ffdeb8ad3fb502e5effed62cf5eb9dbc480b
Robert Fischer <robert.fischer@smokejumperit.com>
2012-07-01 18:29:09 -0400

    Remove the jars and move that stuff into BuildConfig

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -10,13 +10,14 @@ grails.project.dependency.resolution = {
     }
     log "warn" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
     repositories {
+				mavenLocal()
+				ebr()
         grailsPlugins()
         grailsHome()
         grailsCentral()
 
         // uncomment the below to enable remote dependency resolution
         // from public Maven repositories
-        mavenLocal()
         mavenCentral()
         //mavenRepo "http://snapshots.repository.codehaus.org"
         //mavenRepo "http://repository.codehaus.org"
@@ -27,5 +28,13 @@ grails.project.dependency.resolution = {
         // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes eg.
 
         // runtime 'mysql:mysql-connector-java:5.1.13'
+			compile 'org.apache.james:apache-mime4j:0.7.2'
+			compile 'org.apache.lucene:lucene-core:3.6.0'
+			compiile 'org.apache.lucene:lucene-queries:3.6.0'
+      runtime 'postgresql:postgresql:9.1-901.jdbc4'
+			
     }
+		plugins {
+			compile ":navigation:1.3.2"
+		}
 }
