1d9c3693cf55e69a6313439a8417f99bcdb7c399
Phillip Rhodes (mindcrime) <motley.crue.fan@gmail.com>
2014-01-25 17:47:55 -0500

    starts converting to using Solr over HTTP instead of embedding Lucene directly

--- grails-app/services/org/fogbeam/heceta/SolrSearchService.groovy
+++ grails-app/services/org/fogbeam/heceta/SolrSearchService.groovy
@@ -1,8 +1,12 @@
 
 package org.fogbeam.heceta
 
+import org.apache.solr.client.solrj.SolrQuery
 import org.apache.solr.client.solrj.SolrServer
 import org.apache.solr.client.solrj.impl.HttpSolrServer
+import org.apache.solr.client.solrj.response.QueryResponse
+import org.apache.solr.common.SolrDocumentList
+import org.apache.solr.common.SolrDocument
 
 class SolrSearchService 
 {
@@ -10,11 +14,28 @@ class SolrSearchService
 	SolrServer server = new HttpSolrServer( url );
 	
 	
-	public List<String> search( String query )
+	public List<String> search( String queryString )
 	{
 		List<String> results = new ArrayList<String>();
+
+		SolrQuery query = new SolrQuery();
+		query.setQuery( queryString );
 		
-		// TODO: search using Solr
+		// query.addSortField( "price", SolrQuery.ORDER.asc );
+	
+		// Query the server
+	
+		QueryResponse rsp = server.query( query );
+	
+		// Get the results
+	
+		SolrDocumentList docs = rsp.getResults();
+		
+		for( SolrDocument doc : docs )
+		{
+			String result = doc.getFieldValue("name");
+			results.add( result );
+		}
 		
 		return results;
 	}
