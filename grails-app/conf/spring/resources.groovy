2baaf80d3bb92909220c348ca9b0b74ae68e5ce0
Phillip Rhodes <motley.crue.fan@gmail.com>
2017-11-18 12:00:31 -0500

    add ConnectionFactory bean for JMS

--- grails-app/conf/spring/resources.groovy
+++ grails-app/conf/spring/resources.groovy
@@ -1,3 +1,10 @@
 // Place your Spring DSL code here
 beans = {
+	
+	jmsConnectionFactory(org.springframework.jndi.JndiObjectFactoryBean) {
+		jndiName="ConnectionFactory"
+		jndiEnvironment=["java.naming.factory.initial":"org.jnp.interfaces.NamingContextFactory",
+			"java.naming.provider.url":"localhost:1099"]
+		}
+
 }
